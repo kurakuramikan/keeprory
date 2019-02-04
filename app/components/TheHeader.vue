efadd8865c81db938edcc62ab48edcc53ed2dde8
Masayuki Kurahashi <m.kurahashi@ohako-inc.jp>
2019-02-04 15:29:20 +0900

    [Add] Add global header
    - Add global header

---
+++ app/components/TheHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="global-header">
+    <h1 class="global-header__title">keeprory</h1>
+  </div>
+</template>
+
+<style scoped>
+.global-header {
+  width: 100%;
+  padding: 0 32px;
+  box-shadow: 0px 8px 8px -8px #cccccc;
+}
+
+.global-header__title {
+  line-height: 40px;
+  text-align: left;
+  font-size: 18px;
+  color: #808080;
+}
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,8 @@ module.exports = {
 
   // Global CSS
   css: [
-    'element-ui/lib/theme-chalk/index.css'
+    'element-ui/lib/theme-chalk/index.css',
+    '~/assets/reset.css'
   ],
 
   // Plugins to load before mounting the App
