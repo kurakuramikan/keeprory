179cee7d9c0e4610b39d2c151770cb68878a7dfe
Masayuki Kurahashi <m.kurahashi@ohako-inc.jp>
2019-02-04 12:59:39 +0900

    [Add] Add nuxt template on docker
    - Add nuxt template on docker

---
+++ nuxt.config.js
@@ -0,0 +1,52 @@
+const pkg = require('./package')
+
+
+module.exports = {
+  mode: 'spa',
+  srcDir: 'app',
+
+  // Headers of the page
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Customize the progress-bar color
+  loading: { color: '#ccc' },
+
+  // Global CSS
+  css: [
+    'element-ui/lib/theme-chalk/index.css'
+  ],
+
+  // Plugins to load before mounting the App
+  plugins: [
+    '@/plugins/element-ui'
+  ],
+
+  // Nuxt.js modules
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    '@nuxtjs/pwa'
+  ],
+  // Axios module configuration
+  axios: {
+    // See https://github.com/nuxt-community/axios-module#options
+  },
+
+  // Build configuration
+  build: {
+    // You can extend webpack config here
+    extend(config, ctx) {
+
+    }
+  }
+}
